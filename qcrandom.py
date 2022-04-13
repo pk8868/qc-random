4320719a458bb4178aeaa7aa7a335c4a7adcb764
pk8868 <umiemxd@gmail.com>
2022-04-13 10:28:52 +0200

    change fabs to abs and fix accuracy exception

--- qcrandom.py
+++ qcrandom.py
@@ -2,7 +2,7 @@ from qiskit import *
 import math
 
 def RNG(left, right, accuracy=16):
-    if accuracy < 1:
+    if accuracy <= 1:
         raise Exception("Accuracy must be higher than 1!")
     if left >= right:
         raise Exception("Left must be lower than right!")
@@ -10,7 +10,7 @@ def RNG(left, right, accuracy=16):
     qr = QuantumRegister(1)
     cr = ClassicalRegister(accuracy)
     circuit = QuantumCircuit(qr, cr)
-    
+
     for j in range(accuracy):
         circuit.h(0)
         circuit.measure(0, j)
@@ -20,9 +20,9 @@ def RNG(left, right, accuracy=16):
     data = job.result().get_memory()
 
     ret = int(data[0], 2)
-    rrange = math.fabs(right - left)
+    rrange = abs(right - left)
     ret = ret / (2**accuracy - 1)
     ret = ret * rrange
     ret += left
     return ret
-
+    
